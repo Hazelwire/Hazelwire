a44409c41dfa24b253647e031b1a06232e848102
Maarten Rijke <m.rijke@student.utwente.nl>
2011-04-26 21:31:06 +0200

    Started working on the ClientHandler implementation. Made the flagserver; core functionality still needs to be implemented, only global flow is done. Created the DatabaseHandler. This will take care of the communication between the sqlite database and the python services using it (the flagserver and the sanitychecker). Still is very alpha, didn't even test it. Only the basic server implementation is tested, it is multithreaded: each new client gets a new thread.
    Signed-off-by: Maarten Rijke <m.rijke@student.utwente.nl>

---
+++ Mainframe/src/ClientHandler/flagserver.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+
+import socket
+import threading
+import SocketServer
+from DatabaseHandler import database 
+
+# Modules are represented by an array of dictionaries with the keys ModuleName and NumberOfFlags.
+modules = []
+
+class ThreadedTCPRequestHandler(SocketServer.StreamRequestHandler):
+
+    def handle(self):
+        self.data = self.rfile.readline().strip()
+        if self.data == "REQFLAGS":
+            # * Check client IP to see if already requested, if yes: return REQFAIL
+            # * Generate number of flags that are needed according to the manifest
+            # * Send the flags one by one separated by a new module
+            if database.checkClientIP(self.client_address):
+                #Client already requested flags in the past
+                response = "REQFAIL"
+                self.request.send(response)
+                return
+            else:
+                #Client has no flags assigned to him, generate some.
+                for module in modules:
+                    self.request.send("NEWMODULE")
+                    self.request.send("MODNAME " + module['name'])
+                    flags = generateFlags(module['numberOfFlags'], clientIP)
+                    for flag in flags:
+                        self.request.send("FLAG " + flag)
+                self.request.send("ENDFLAGS")
+        elif self.data == "REQSHUTDOWN":
+            if self.client_address == "127.0.0.1": #can only be sent from localhost
+                self.server.shutdown()
+            return
+
+class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+    pass
+
+def readManifest(manifest):
+    """Check the manifest for list of modules and how many flags must be created"""
+    pass
+
+
+def generateFlags(number, clientIP):
+    """Takes a number of flags that need to be generated for clientIP. Adds them to the database"""
+    flags = []
+    for x in range(number):
+        flag.append("some generated flag")#generate flag
+    while not database.addFlags(flags, clientIP): #keep on generating flags when some flag already existed
+        for x in range(number):
+            flag.append("some generated flag")#generate flag
+    
+
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 9999
+
+    server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
+    ip, port = server.server_address
+
+    # Start a thread with the server -- that thread will then start one
+    # more thread for each request
+    server_thread = threading.Thread(target=server.serve_forever)
+    # Exit the server thread when the main thread terminates
+    server_thread.start()
+    print "Server loop running in thread:", server_thread.getName()
+
---
+++ Mainframe/src/DatabaseHandler/database.py
@@ -0,0 +1,32 @@
+import sqlite3
+""" Handler for communication with the Database."""
+
+def connect():
+    global conn
+    conn = sqlite3.connect("derp")
+    return conn.cursor()
+
+def disconnect():
+    global conn
+    conn.close()
+
+def checkClientIP(clientIP):
+    c = connect()
+    c.execute("SELECT * FROM flags WHERE clientip=?", clientIP)
+    c.close()
+    disconnect()
+    return len(c) == 0
+
+def addFlags(flags, clientIP): 
+    c = connect()
+    for flag in flags: # check if a flag already is in the database 
+        c.execute("SELECT * FROM flags WHERE flag=?", flag)
+        if len(c) != 0:
+            return False
+    for flag in flags: #actually add the flags
+        c.execute("INSERT INTO flags ") #TODO: add some actual SQL here.
+    c.commit()
+    c.close()
+    disconnect()
+    
+   
