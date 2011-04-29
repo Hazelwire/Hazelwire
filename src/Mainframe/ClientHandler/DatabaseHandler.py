7b2f105653b3feae81e3abcc1e7f9d1ff6c38901
Maarten Rijke <m.rijke@student.utwente.nl>
2011-04-30 00:14:56 +0200

    Implemented communication with the databse. Only thing left to fix is keeping track of the points of the flags belonging to a certain module in the modules table.
    Signed-off-by: Maarten Rijke <m.rijke@student.utwente.nl>

--- src/Mainframe/ClientHandler/DatabaseHandler.py
+++ src/Mainframe/ClientHandler/DatabaseHandler.py
@@ -1,34 +1,48 @@
 import sqlite3
 """ Handler for communication with the Database."""
 
+db = "test.db"
+
 def connect():
     global conn
-    conn = sqlite3.connect("derp")
+    conn = sqlite3.connect(db)
     return conn.cursor()
 
 def disconnect():
     global conn
+    conn.commit()
     conn.close()
 
 def checkClientIP(clientIP):
-    return False #just for debugging
     c = connect()
-    c.execute("SELECT * FROM flags WHERE clientip=?", clientIP)
-    c.close()
+    c.execute("SELECT * FROM flags WHERE team_id = (SELECT id FROM teams WHERE VMip=?);", [clientIP])
+    res = c.fetchall()
     disconnect()
-    return len(c) == 0
+    return len(res) != 0
 
-def addFlags(flags, clientIP): 
-    return True #just for debugging
+def addFlags(modulename, flags, clientIP): 
     c = connect()
     for flag in flags: # check if a flag already is in the database 
-        c.execute("SELECT * FROM flags WHERE flag=?", flag)
-        if len(c) != 0:
+        c.execute("SELECT * FROM flags WHERE flag=?", [flag['flag']])
+        if len(c.fetchall()) != 0:
             return False
     for flag in flags: #actually add the flags
-        c.execute("INSERT INTO flags ") #TODO: add some actual SQL here.
-    c.commit()
+        c.execute("INSERT INTO flags VALUES (?, (SELECT id FROM modules WHERE name=?), (SELECT id FROM teams WHERE VMip=?), ?, ?);", [flags.index(flag)+1,modulename, clientIP, flags[flags.index(flag)]['points'], flag['flag']]) #TODO: add some actual SQL here.
     c.close()
     disconnect()
-    
-   
+    return True
+
+def getModuleInfo(): #TODO: get the points info as well, not yet in the database: need to figure out how to store.
+    res = []
+    c = connect()
+    c.execute("SELECT * FROM modules;")
+    for module in c.fetchall():
+        res.append({'name':module[1], 'numFlags':module[2], 'basepath':module[3], 'deployscript':module[4], 'points':[4,12,3,12,23,41]}) #points are bogus
+    return res
+
+if __name__ == "__main__":
+    assert checkClientIP('10.0.8.1') == True
+    assert checkClientIP('10.0.10.1') == False
+    assert addFlags("pwnjebox", [{'flag':'FLGZ0sRrUGvTLLaHqv8dUwy3HPgDo8y5ZCweGvCt9VyuXCus4UmUhzqeB9FFr6c7', 'points':41}], '10.0.8.1') == False
+    print addFlags("pwnjebox", [{'flag':'FLGZ0sRrUGvTLLaHqv8dUwy3HLoLDERPZCweGvCt9VyuXCus4UmUhzqeB9FFr6c7', 'points':83}], '10.0.8.1')
+    print getModuleInfo()
--- src/Mainframe/ClientHandler/FlagAdministration.py
+++ src/Mainframe/ClientHandler/FlagAdministration.py
@@ -3,7 +3,7 @@ import SocketServer
 import DatabaseHandler
 import string, random
 
-# Modules are represented by an array of dictionaries with the keys ModuleName and NumberOfFlags, BasePath, InstallScript path
+# Modules are represented by an array of dictionaries with the keys ModuleName and numFlags, array with points of Flags, BasePath, InstallScript path
 modules = []
 
 class ThreadedTCPRequestHandler(SocketServer.StreamRequestHandler):
@@ -16,7 +16,7 @@ class ThreadedTCPRequestHandler(SocketServer.StreamRequestHandler):
             # * Generate number of flags that are needed according to the manifest
             # * Send the flags one by one separated by a new module
             print "Got request from " + self.client_address[0]
-            if DatabaseHandler.checkClientIP(self.client_address):
+            if DatabaseHandler.checkClientIP(self.client_address[0]):
                 #Client already requested flags in the past
                 response = "REQFAIL\n"
                 self.wfile.write(response)
@@ -28,9 +28,9 @@ class ThreadedTCPRequestHandler(SocketServer.StreamRequestHandler):
                     self.wfile.write("MODNAME " + module['name']+'\n')
                     self.wfile.write("BASEPATH " + module['basepath'] + '\n')
                     self.wfile.write("DEPLOYSCRIPT " + module['deployscript'] + '\n')
-                    module['flags'] = generateFlags(module['name'], module['numberOfFlags'], self.client_address[0])
+                    module['flags'] = generateFlags(module['name'], module['numFlags'], module['points'], self.client_address[0])
                     for flag in module['flags']:
-                        self.wfile.write("FLAG " + flag+'\n')
+                        self.wfile.write("FLAG " + flag['flag']+'\n')
                     self.wfile.write("ENDMODULE\n")
                 self.wfile.write("ENDFLAGS\n")
                 print "Sent " + self.client_address[0] + " some flags."
@@ -43,19 +43,20 @@ class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
     pass
 
 def getModules():
-    """Check the datanase for list of modules, how many flags must be created for each module and the paths """
-    pass
+    """Check the datanase for list of modules, how many flags must be created for each module and the paths + flag points """
+    global modules
+    modules = DatabaseHandler.getModuleInfo()
 
 
-def generateFlags(modulename, number, clientIP):
+def generateFlags(modulename, number, points, clientIP):
     """Takes a number of flags that need to be generated for clientIP. Adds them to the database"""
     while True:
         flags = []
         for x in range(number):
-            flags.append("FLG")
+            flags.append({'flag':"FLG", 'points': points[x]})
             for x in range(61):
-                flags[-1] += random.choice(string.letters+string.digits) #choose a random letter or digit
-        if DatabaseHandler.addFlags(flags, clientIP): break #if succesfully added, we're done.
+                flags[-1]['flag'] += random.choice(string.letters+string.digits) #choose a random letter or digit
+        if DatabaseHandler.addFlags(modulename, flags, clientIP): break #if succesfully added, we're done.
     return flags
 
 def startServer(host,port):
@@ -68,7 +69,8 @@ def startServer(host,port):
     
 if __name__ == "__main__":
     HOST, PORT = "localhost", 9999
-    modules = [{'name':'test1', 'flags':[], 'numberOfFlags':5, 'basepath':'/exploit1/', 'deployscript':'deploy/script.py'},{'name':'test2', 'flags':[], 'numberOfFlags':2, 'basepath':'/exploit2/', 'deployscript':'deploy/script.py'},{'name':'test3', 'flags':[], 'numberOfFlags':4, 'basepath':'/exploit3/', 'deployscript':'deploy/script.py'}]
+    #modules = [{'name':'test1', 'flags':[], 'numFlags':5, 'points':[13,2,1,3,4],'basepath':'/exploit1/', 'deployscript':'deploy/script.py'},{'name':'test2', 'flags':[], 'numFlags':2, 'points':[42,23,12,41,1], 'basepath':'/exploit2/', 'deployscript':'deploy/script.py'},{'name':'test3', 'flags':[], 'numFlags':4, 'points':[13,2,1,3,4], 'basepath':'/exploit3/', 'deployscript':'deploy/script.py'}]
+    getModules()
     startServer(HOST, PORT)
     print "Started flag administration service on port " + str(PORT)
-
+    print modules
