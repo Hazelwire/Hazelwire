62a93e57af8535cf1166925b3b9300920af7ae60
Maarten Rijke <m.rijke@student.utwente.nl>
2011-05-03 19:47:49 +0200

    Implemented simple service to start OpenVPN instances and enable/disable kernel routing

---
+++ src/Mainframe/WebInterfaces/lib/admin/OpenVPNService.py
@@ -0,0 +1,23 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(("127.0.0.1",9999))
+s.listen(1)
+running = True
+
+while running:
+    conn, addr = socket.accept()
+    data = conn.recv(1024)
+    if data.startswith("STARTVPN "):
+        conffile = data.split(' ')[1]
+        os.system('/usr/sbin/openvpn ' + conffile)
+        conn.close()
+    elif data == "STARTKROUTING":
+        os.system('sysctl net.ipv4.ip_forward=1')
+    elif data == "STOPKROUTING":
+        os.system('sysctl net.ipv4.ip_forward=0')
+    elif data == "STOPSERVICE":
+        conn.close()
+        s.close()
+        running = False
+
