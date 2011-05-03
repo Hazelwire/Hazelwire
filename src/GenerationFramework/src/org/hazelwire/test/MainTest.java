425752e8f0cfc9103d40014f4c52895d3cea5870
Tim Strijdhorst <t.strijdhorst@student.utwente.nl>
2011-05-03 14:30:19 +0200

    Begonnen met het bouwen van de modules. Files worden gelezen uit een path nu moeten de zip's nog geëxtract worden en de juiste interne classes voor de modulen worden geinitieerd.

--- src/GenerationFramework/src/org/hazelwire/test/MainTest.java
+++ src/GenerationFramework/src/org/hazelwire/test/MainTest.java
@@ -1,9 +1,52 @@
 package org.hazelwire.test;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class MainTest
 {
 	public static void main(String args[])
 	{
-		System.out.println("LOL WUT?");
+		try
+		{
+			String path = "/home/shokora/";
+			
+			File fileTest = new File(path);
+			
+			if(fileTest.isFile())
+			{
+				FileInputStream test = new FileInputStream(path);
+				InputStreamReader testReader = new InputStreamReader(test,"UTF-8");
+				Scanner bla = new Scanner(test);
+				
+				while(bla.hasNext())
+				{
+					System.out.print(bla.next());
+				}
+			}
+			else
+			{
+				String[] fileList = fileTest.list();
+				
+				for(int i=0;i<fileList.length;i++)
+				{
+					System.out.println(fileList[i]);
+				}
+			}
+		}
+		catch (FileNotFoundException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch (Exception e)
+		{
+			
+		}
+		
+		
 	}
 }
