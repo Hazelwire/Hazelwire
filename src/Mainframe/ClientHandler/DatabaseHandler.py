146987f9f7832e66bd4440978a4bf3bfe5faf0b0
Maarten Rijke <m.rijke@student.utwente.nl>
2011-05-07 00:46:29 +0200

    Transformed DatabaseHandler into a class, for easy testing. Next up: creating a unittest for DatabaseHandler
    Signed-off-by: Maarten Rijke <m.rijke@student.utwente.nl>

--- src/Mainframe/ClientHandler/DatabaseHandler.py
+++ src/Mainframe/ClientHandler/DatabaseHandler.py
@@ -1,61 +1,70 @@
 import sqlite3
 """ Handler for communication with the Database."""
 
-db = "test.db"
-
-def connect():
-    global conn
-    conn = sqlite3.connect(db)
-    return conn.cursor()
-
-def disconnect():
-    global conn
-    conn.commit()
-    conn.close()
-
-def checkClientIP(clientIP):
-    c = connect()
-    c.execute("SELECT * FROM flags WHERE team_id = (SELECT id FROM teams WHERE VMip=?);", [clientIP])
-    res = c.fetchall()
-    disconnect()
-    return len(res) != 0
-
-def addFlags(modulename, flags, clientIP): 
-    c = connect()
-    for flag in flags: # check if a flag already is in the database 
-        c.execute("SELECT * FROM flags WHERE flag=?", [flag])
-        if len(c.fetchall()) != 0:
-            return False
-    for flag in flags: #actually add the flags
-        c.execute("INSERT INTO flags VALUES (?, (SELECT id FROM modules WHERE name=?), (SELECT id FROM teams WHERE VMip=?), ?);", [flags.index(flag)+1,modulename, clientIP, flag]) #TODO: add some actual SQL here.
-    c.close()
-    disconnect()
-    return True
-
-def addModuleInfo(modules):
-    c = connect()
-    for module in modules:
-        c.execute("INSERT INTO modules VALUES (?, ?, ?, ?, ?);",[None, module['name'], module['numFlags'], module['basepath'], module['deployscript']])
-    c.close()
-    disconnect()
-    c = connect()
-    for module in modules:
-        for flag in module['flagpoints']:
-            c.execute("INSERT INTO flagpoints VALUES (?, ?, ?);", [module['flagpoints'].index(flag)+1, modules.index(module)+1, flag])
-    c.close()
-    disconnect()
+class DatabaseHandler:
+        
+    def __init__(self,db="test.db"):
+        self.db = db
+        
+    def connect(self):
+        self.conn = sqlite3.connect(self.db)
     
-def getModuleInfo():
-    res = []
-    c = connect()
-    c.execute("SELECT * FROM modules;")
-    for module in c.fetchall():
-        res.append({'name':module[1], 'numFlags':module[2], 'basepath':module[3], 'deployscript':module[4]})
-    return res
+    def disconnect(self):
+        self.conn.commit()
+        self.conn.close()
+    
+    def checkClientIP(self,clientIP):
+        self.connect()
+        c = self.conn.cursor()
+        c.execute("SELECT * FROM flags WHERE team_id = (SELECT id FROM teams WHERE VMip=?);", [clientIP])
+        res = c.fetchall()
+        c.close()
+        self.disconnect()
+        return len(res) != 0
+    
+    def addFlags(self, modulename, flags, clientIP): 
+        self.connect()
+        c = self.conn.cursor()
+        for flag in flags: # check if a flag already is in the database 
+            c.execute("SELECT * FROM flags WHERE flag=?", [flag])
+            if len(c.fetchall()) != 0:
+                return False
+        for flag in flags: #actually add the flags
+            c.execute("INSERT INTO flags VALUES (?, (SELECT id FROM modules WHERE name=?), (SELECT id FROM teams WHERE VMip=?), ?);", [flags.index(flag)+1,modulename, clientIP, flag]) #TODO: add some actual SQL here.
+        c.close()
+        self.disconnect()
+        return True
+    
+    def addModuleInfo(self,modules):
+        self.connect()
+        c = self.conn.cursor()
+        for module in modules:
+            c.execute("INSERT INTO modules VALUES (?, ?, ?, ?, ?);",[None, module['name'], module['numFlags'], module['basepath'], module['deployscript']])
+        c.close()
+        self.disconnect()
+        self.connect()
+        c = self.conn.cursor()
+        for module in modules:
+            for flag in module['flagpoints']:
+                c.execute("INSERT INTO flagpoints VALUES (?, ?, ?);", [module['flagpoints'].index(flag)+1, modules.index(module)+1, flag])
+        c.close()
+        self.disconnect()
+        
+    def getModuleInfo(self):
+        res = []
+        self.connect()
+        c = self.conn.cursor()
+        c.execute("SELECT * FROM modules;")
+        for module in c.fetchall():
+            res.append({'name':module[1], 'numFlags':module[2], 'basepath':module[3], 'deployscript':module[4]})
+        self.disconnect()
+        return res
+    
 
 if __name__ == "__main__":
-    assert checkClientIP('10.0.8.1') == True
-    assert checkClientIP('10.0.10.1') == False
-    assert addFlags("pwnjebox", [{'flag':'FLGZ0sRrUGvTLLaHqv8dUwy3HPgDo8y5ZCweGvCt9VyuXCus4UmUhzqeB9FFr6c7', 'points':41}], '10.0.8.1') == False
-    print addFlags("pwnjebox", [{'flag':'FLGZ0sRrUGvTLLaHqv8dUwy3HLoLDERPZCweGvCt9VyuXCus4UmUhzqeB9FFr6c7', 'points':83}], '10.0.8.1')
-    print getModuleInfo()
+    db = DatabaseHandler("test.db")
+    assert db.checkClientIP('10.0.8.1') == True
+    assert db.checkClientIP('10.0.10.1') == False
+    assert db.addFlags("pwnjebox", ['FLGZ0sRrUGvTLLaHqv8dUwy3HPgDo8y5ZCweGvCt9VyuXCus4UmUhzqeB9FFr6c7'], '10.0.8.1') == False
+    print db.addFlags("pwnjebox", ['FLGZ0sRrUGvTLLaHqv8dUwy3HLoLDERPZCweGvCt9VyuXCus4UmUhzqeB9FFr6c7'], '10.0.8.1')
+    print db.getModuleInfo()
