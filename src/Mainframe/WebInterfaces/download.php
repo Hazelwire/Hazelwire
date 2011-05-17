0cb26e666bac6d0a0675546a105f003bd2d18b16
Daniel van der Steeg <d.p.m.h.vandersteeg@student.utwente.nl>
2011-05-17 15:54:50 +0200

    Fixed another bug with regards to downlading files. Signed-off-by: Daniel van der Steeg <d.p.m.h.vandersteeg@student.utwente.nl>

--- src/Mainframe/WebInterfaces/download.php
+++ src/Mainframe/WebInterfaces/download.php
@@ -3,9 +3,9 @@ include_once 'config.php';
 include_once 'lib/etc_funcs.php';
 
 if(strcmp($_SERVER['REMOTE_ADDR'], '127.0.0.1') === 0 || ip_in_range($_SERVER['REMOTE_ADDR'], $config['admin_ip_range'])){
-    if(!ctype_alnum($_GET['team']))
+    if(isset($_GET['team']) && !ctype_alnum($_GET['team']))
             exit;
-    if($_GET['type'] == "tkey"){
+    if(isset($_GET['team']) && isset($_GET['type']) && $_GET['type'] == "tkey"){
         
         $file = $config['RSA_location'] . "keys/" . $_GET['team'] . ".key";
         
@@ -14,7 +14,7 @@ if(strcmp($_SERVER['REMOTE_ADDR'], '127.0.0.1') === 0 || ip_in_range($_SERVER['R
         
         loadFile($file);
         
-    }elseif($_GET['type'] == 'tcert'){
+    }elseif(isset($_GET['team']) && isset($_GET['type']) && $_GET['type'] == 'tcert'){
         
         $file = $config['RSA_location'] . "keys/" . $_GET['team'] . ".crt";
         
@@ -23,7 +23,7 @@ if(strcmp($_SERVER['REMOTE_ADDR'], '127.0.0.1') === 0 || ip_in_range($_SERVER['R
         
         loadFile($file);    
         
-    }elseif($_GET['type'] == 'vkey'){
+    }elseif(isset($_GET['team']) && isset($_GET['type']) && $_GET['type'] == 'vkey'){
         $file = $config['RSA_location'] . "keys/" . $_GET['team'] . "_vm.key";
         
         if(!file_exists($file))
@@ -31,7 +31,7 @@ if(strcmp($_SERVER['REMOTE_ADDR'], '127.0.0.1') === 0 || ip_in_range($_SERVER['R
         
         loadFile($file);
         
-    }elseif($_GET['type'] == 'vcert'){
+    }elseif(isset($_GET['team']) && isset($_GET['type']) && $_GET['type'] == 'vcert'){
         
         $file = $config['RSA_location'] . "keys/" . $_GET['team'] . "_vm.crt";
         
@@ -39,7 +39,7 @@ if(strcmp($_SERVER['REMOTE_ADDR'], '127.0.0.1') === 0 || ip_in_range($_SERVER['R
             exit;
         
         loadFile($file);
-    }elseif($_GET['type'] == 'ca'){
+    }elseif(isset($_GET['type']) && $_GET['type'] == 'ca'){
         
         $file = $config['RSA_location'] . "keys/ca.crt";
         
@@ -47,7 +47,7 @@ if(strcmp($_SERVER['REMOTE_ADDR'], '127.0.0.1') === 0 || ip_in_range($_SERVER['R
             exit;
         
         loadFile($file);
-    }elseif($_GET['type'] == 'vpnconf'){
+    }elseif(isset($_GET['team']) && isset($_GET['type']) && $_GET['type'] == 'vpnconf'){
 
         $file = $config['openvpn_location'] . $_GET['team'] . "_client.conf";
 
