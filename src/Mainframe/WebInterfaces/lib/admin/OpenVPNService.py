83adfc1a691c845d18a265c4a86bf7bd496c6c26
Maarten Rijke <m.rijke@student.utwente.nl>
2011-05-17 16:44:20 +0200

    Fixed the OpenVPNService not starting the openvpn instances threaded.
    Signed-off-by: Maarten Rijke <m.rijke@student.utwente.nl>

--- src/Mainframe/WebInterfaces/lib/admin/OpenVPNService.py
+++ src/Mainframe/WebInterfaces/lib/admin/OpenVPNService.py
@@ -1,4 +1,4 @@
-import socket
+import socket, os, subprocess
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(("127.0.0.1",10000))
@@ -6,11 +6,13 @@ s.listen(1)
 running = True
 
 while running:
-    conn, addr = socket.accept()
-    data = conn.recv(1024)
+    conn, addr = s.accept()
+    data = conn.recv(1024).strip()
+    print "Got %s from %s" % (data, addr[0])
     if data.startswith("STARTVPN "):
         conffile = data.split(' ')[1]
-        os.system('/usr/sbin/openvpn ' + conffile)
+	print 'Calling /usr/sbin/openvpn ' + conffile
+        subprocess.Popen('/usr/sbin/openvpn ' + conffile, shell=True)
         conn.close()
     elif data == "STARTKROUTING":
         os.system('sysctl net.ipv4.ip_forward=1')
