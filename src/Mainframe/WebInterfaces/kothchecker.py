72ce5f92d1256d9c99dce82cbe66618ff437316e
Maarten Rijke <maartenrijke@gmail.com>
2011-12-15 17:43:10 +0100

    Added King Of The Hill checker, put it in a cronjob every minute or so
    Signed-off-by: Maarten Rijke <maartenrijke@gmail.com>

---
+++ src/Mainframe/WebInterfaces/kothchecker.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+from HTMLParser import HTMLParser
+import urllib2, sqlite3, time
+
+class MLStripper(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.fed = []
+    def handle_data(self, d):
+        self.fed.append(d)
+    def get_data(self):
+        return ''.join(self.fed)
+
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
+
+def read_team_from_koth_page(url):
+    page = urllib2.urlopen(url).readlines()
+    for line in page:
+	if line.find('SYSCORE') > 0:
+            teamline = line
+    if not teamline:
+	return False
+    team = strip_tags(''.join(teamline.split('-')[1:]).strip())
+    return team.strip(' ')
+
+def give_team_score(team):
+    db = sqlite3.connect('Hazelwire.sqlite')
+    c = db.cursor()
+    c.execute("insert into scores values ( (SELECT id FROM teams WHERE name=?), 0, 'KOTH', ?, 1, 0);", [team, int(time.time())])
+    db.commit()
+    c.close()
+    db.close()
+
+if __name__ == "__main__":
+    KOTH_IP = "10.1.1.137"
+    team = read_team_from_koth_page('http://'+KOTH_IP)
+    give_team_score(team)
